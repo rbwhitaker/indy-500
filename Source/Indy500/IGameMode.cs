2c78f39c04720058d1019dcd7680c27e3fae075d
Joel <moolick@outlook.com>
2019-12-28 16:41:21 -0600

    Level loading is now done from file.

--- Source/Indy500/IGameMode.cs
+++ Source/Indy500/IGameMode.cs
@@ -4,6 +4,8 @@ namespace Indy500
 {
     public interface IGameMode
     {
+        // Maybe have a CreateAICar here somewhere to return a custom ai implementation per-game mode?
+        // If we go this route, we can remove that nasty reference Update has to Race.
         void Update(GameTime gameTime, Race race);
         bool IsOver();
         Car Winner { get; }
