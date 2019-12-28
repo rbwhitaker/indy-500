fe18f1fb996b8dd7a94c2d97a2734a631d072916
RB Whitaker <rbwhitaker@outlook.com>
2019-12-28 13:46:33 -0700

    Moved some stuff around to make it easier to get new scenes into the mix.

--- Source/Indy500/Indy500Game.cs
+++ Source/Indy500/Indy500Game.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Indy500.SceneManagement;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -9,27 +10,26 @@ namespace Indy500
     public class Indy500Game : Game
     {
         private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-        private Race activeRace;
-        private IRenderer renderer;
+        private InGameScene inGameScene;
         
         public Indy500Game()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
         }
 
         protected override void Initialize()
         {
-            base.Initialize();
             GameSettings.Initialize();
+            inGameScene = new InGameScene();
             graphics.IsFullScreen = GameSettings.IsFullScreen;
             graphics.PreferredBackBufferWidth = GameSettings.Width;
             graphics.PreferredBackBufferHeight = GameSettings.Height;
             graphics.ApplyChanges();
             Window.AllowUserResizing = true;
             Window.ClientSizeChanged += WindowSizeChanged;
-            activeRace = new Race(CreateSimpleTrack(), CreatePlayers(), new RaceMode(3, 24, 40, 18, 5));
+            base.Initialize();
         }
 
         private void WindowSizeChanged(object sender, EventArgs e)
@@ -39,45 +39,9 @@ namespace Indy500
             GameSettings.SaveConfigFile();
         }
 
-        private Track CreateSimpleTrack(int rows = 24, int columns = 40, int islandWidth = 18, int islandHeight = 5)
-        {
-            Track track = new Track(rows, columns);
-
-            // Set up the outside edge.
-            for (int x = 0; x < columns; x++)
-            {
-                track[0, x] = TrackTileType.Dirt;
-                track[rows - 1, x] = TrackTileType.Dirt;
-            }
-
-            for (int y = 0; y < rows; y++)
-            {
-                track[y, 0] = TrackTileType.Dirt;
-                track[y, columns - 1] = TrackTileType.Dirt;
-            }
-
-            // Set up the "island" in the middle
-            for (int x = (columns - islandWidth) / 2; x < (columns + islandWidth) / 2; x++)
-                for (int y = (rows - islandHeight) / 2; y < (rows + islandHeight) / 2; y++)
-                    track[y, x] = TrackTileType.Dirt;
-
-            return track;
-        }
-
-        private IEnumerable<Car> CreatePlayers()
-        {
-            return new List<Car>
-            {
-                new Car(new ControlledPlayer()),
-                new Car(new DoNothingPlayer())
-            };
-        }
-
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            renderer = new Simple2DRenderer();
-            renderer.LoadContent(GraphicsDevice, Content);
+            inGameScene.LoadContent(GraphicsDevice, Content);
         }
 
         protected override void UnloadContent()
@@ -86,10 +50,7 @@ namespace Indy500
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            activeRace.Update(gameTime);
+            inGameScene.Update(gameTime);
             
             base.Update(gameTime);
         }
@@ -98,7 +59,7 @@ namespace Indy500
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            renderer.Draw(activeRace, gameTime);
+            inGameScene.Draw(gameTime);
 
             base.Draw(gameTime);
         }
