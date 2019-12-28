d6466659629b3a9370a49ba61036355981d15089
RB Whitaker <rbwhitaker@outlook.com>
2019-12-28 10:07:01 -0700

    Added a little more to DoNothingPlayer to drive in random circles.

--- Source/Indy500/DoNothingPlayer.cs
+++ Source/Indy500/DoNothingPlayer.cs
@@ -4,9 +4,17 @@ namespace Indy500
 {
     internal class DoNothingPlayer : IPlayer
     {
+        private bool leftOrRight;
+        private static bool nextLeftOrRight = true;
+
+        public DoNothingPlayer()
+        {
+            leftOrRight = nextLeftOrRight;
+            nextLeftOrRight = !nextLeftOrRight;
+        }
         public PlayerInput Update(GameTime gameTime, Race race, Car carToControl)
         {
-            return new PlayerInput(0, 0);
+            return new PlayerInput(1, leftOrRight ? 1 : 0.05f);
         }
     }
 }
