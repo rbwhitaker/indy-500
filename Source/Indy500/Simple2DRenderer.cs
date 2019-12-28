4f97cd81068ce48181236e2fce2e06d159a12049
RB Whitaker <rbwhitaker@outlook.com>
2019-12-28 09:06:54 -0700

    The background is also now being drawn.

--- Source/Indy500/Simple2DRenderer.cs
+++ Source/Indy500/Simple2DRenderer.cs
@@ -12,20 +12,34 @@ namespace Indy500
     public class Simple2DRenderer : IRenderer
     {
         private Texture2D car;
-        private float tileSize = 10;
+        private Texture2D rectangle;
+        private float tileSize = 20;
         private SpriteBatch spriteBatch;
         
         public void Draw(Race race, GameTime gameTime)
         {
             spriteBatch.Begin();
-            spriteBatch.Draw(car, race.Cars[0].Position * tileSize, null, Color.Red, race.Cars[0].Heading, new Vector2(car.Width / 2f, car.Height / 2f), (1f/car.Width)*tileSize*2, SpriteEffects.None, 0);
+
+            for(int row = 0; row < race.Track.Rows; row++)
+            {
+                for(int column = 0; column < race.Track.Columns; column++)
+                {
+                    spriteBatch.Draw(rectangle, new Rectangle((int)(column * tileSize), (int)(row * tileSize), (int)tileSize, (int)tileSize), race.Track[row, column] == TrackTileType.Dirt ? Color.Green : Color.Gray);
+                }
+            }
+
+            foreach(Car c in race.Cars)
+            {
+                spriteBatch.Draw(car, c.Position * tileSize, null, Color.Red, c.Heading, new Vector2(car.Width / 2f, car.Height / 2f), 1f / car.Width * tileSize * 2, SpriteEffects.None, 0);
+            }
             spriteBatch.End();
         }
 
         public void LoadContent(GraphicsDevice graphicsDevice, ContentManager content)
         {
             spriteBatch = new SpriteBatch(graphicsDevice);
-            car = content.Load<Texture2D>("Car"); 
+            car = content.Load<Texture2D>("Car");
+            rectangle = content.Load<Texture2D>("Rectangle");
         }
     }
 }
