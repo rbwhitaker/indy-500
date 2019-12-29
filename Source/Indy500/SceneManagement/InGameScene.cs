700215277abe8a1e70f2c81137f30327176afefe
RB Whitaker <rbwhitaker@outlook.com>
2019-12-28 18:59:52 -0700

    You can now restart a level (temporarily) by pushing the backspace key.

--- Source/Indy500/SceneManagement/InGameScene.cs
+++ Source/Indy500/SceneManagement/InGameScene.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Indy500.SceneManagement
 {
@@ -18,25 +19,37 @@ namespace Indy500.SceneManagement
 
         public void LoadContent(GraphicsDevice graphicsDevice, ContentManager content)
         {
-            Level level = Level.Parse(System.IO.File.ReadAllText("LevelExample.txt"));
-
-            activeRace = RaceBuilder.FromLevel(level, messageDispatcher);
+            MakeNewLevel();
 
             renderer.LoadContent(graphicsDevice, content);
         }
 
+        private void MakeNewLevel()
+        {
+            Level level = Level.Parse(System.IO.File.ReadAllText("LevelExample.txt"));
+            activeRace = RaceBuilder.FromLevel(level, messageDispatcher);
+        }
+
+        private KeyboardState previousState = new KeyboardState();
         public void Update(GameTime gameTime)
         {
             activeRace.Update(gameTime);
             renderer.Update(gameTime);
+
+            KeyboardState currentState = Keyboard.GetState();
+            if (currentState.IsKeyDown(Keys.Back) && previousState.IsKeyUp(Keys.Back))
+                MakeNewLevel();
+            previousState = currentState;
         }
 
         public void Draw(GameTime gameTime)
         {
             renderer.Draw(activeRace, gameTime);
         }
+
         public void Reset()
         {
+            previousState = new KeyboardState();
         }
     }
 }
